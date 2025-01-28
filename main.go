20babbed4eb9cbeaa224732dc1a3d23769b25652
Connor van Spronssen <info@dobefu.com>
2025-01-28 19:10:50 +0100

    feat: Add a section for the OS icon to the output

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/Dobefu/cli-prompt/cmd/structs"
+	"github.com/Dobefu/cli-prompt/cmd/utils"
+)
+
 func main() {
-	// Empty for now.
+	utils.PrintSection(
+		utils.GetOSIcon(),
+		structs.ColorRGB{R: 0, G: 0, B: 255},
+		structs.ColorRGB{R: 200, G: 200, B: 200},
+	)
 }
