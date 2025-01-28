5d2ff520d15c634c8620186b07e25027a297b59a
Connor van Spronssen <info@dobefu.com>
2025-01-28 19:40:19 +0100

    feat: Add logic to be able to print multiple sections

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Dobefu/cli-prompt/cmd/structs"
 	"github.com/Dobefu/cli-prompt/cmd/utils"
 )
 
+type section struct {
+	content string
+	fg      structs.ColorRGB
+	bg      structs.ColorRGB
+}
+
 func main() {
-	utils.PrintSection(
-		utils.GetOSIcon(),
-		structs.ColorRGB{R: 0, G: 0, B: 255},
-		structs.ColorRGB{R: 200, G: 200, B: 200},
-	)
+	var sections = []section{
+		{
+			utils.GetOSIcon(),
+			structs.ColorRGB{R: 0, G: 0, B: 255},
+			structs.ColorRGB{R: 200, G: 200, B: 200},
+		},
+	}
+
+	numSections := len(sections)
+
+	for i := 0; i < numSections; i++ {
+		utils.PrintSection(sections[i].content, sections[i].fg, sections[i].bg)
+
+		if i == numSections-1 {
+			fmt.Println("")
+		} else {
+			fmt.Print(" ")
+		}
+	}
 }
