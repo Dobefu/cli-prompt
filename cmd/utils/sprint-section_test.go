50228ff573a45e10415868c10f36237eb9b49fba
Connor van Spronssen <info@dobefu.com>
2025-01-30 10:57:35 +0100

    test: Fix the SprintSection test

--- cmd/utils/sprint-section_test.go
+++ cmd/utils/sprint-section_test.go
@@ -21,12 +21,13 @@ func TestSprintSectionSuccess(t *testing.T) {
 	cleanup := setupSprintSectionTest()
 	defer cleanup()
 
-	section := SprintSection(
+	section, sectionLen := SprintSection(
 		"test",
 		structs.ColorRGB{R: 0, G: 0, B: 100},
 		structs.ColorRGB{R: 200, G: 50, B: 0},
 	)
 
+	assert.Equal(t, 6, sectionLen)
 	assert.Contains(t, section, bracketOpen)
 	assert.Contains(t, section, bracketClose)
 	assert.Contains(t, section, "test")
